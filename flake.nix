cda812f84be72ad869f71ec69359c2056e5f5102
Whovian9369 <Whovian9369@gmail.com>
2025-01-13 09:53:57 -0500

    Add flake output to test AppArmor integration easier.

--- flake.nix
+++ flake.nix
@@ -17,6 +17,7 @@
   {
     packages.x86_64-linux = {
       default = pkgs.callPackage ./package.nix {};
+      rp_apparmor = pkgs.callPackage ./package.nix { useAppArmor = true; };
       rp_gtk2 = pkgs.callPackage ./package.nix { build_xfce_plugin = true; };
       rp_gtk3 = pkgs.callPackage ./package.nix { build_gtk3_plugin = true; };
       rp_gtk4 = pkgs.callPackage ./package.nix { build_gtk4_plugin = true; };
